c480c3b99e6fc0f752a9559de062e657036ddee3
Maximilian <mposnikmle@gmail.com>
2023-11-13 16:41:33 -0500

    added header and profile page

---
+++ caretracker/src/components/header-section/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+function Header(props) {
+    return (
+        <>
+            <header className="App-header">
+                <h1>CareTracker</h1>
+            </header>
+        </>
+    );
+}
+
+
+export default Header;
