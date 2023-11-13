517ae2fa1700ad45e8b2375ca11aef48aa5a0275
Dominic Gangi <76754852+djg9@users.noreply.github.com>
2023-11-13 16:18:48 -0500

    Added Auth section

---
+++ caretracker/src/components/authorization-section/Login.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { API_STAFF_LOGIN } from "../../constants/endpoints";
+import { useNavigate } from "react-router-dom";
+
+const Login = (props) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    async function handleSubmit() {
+        try {
+            let myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+
+            const body = {
+                email: email,
+                password: password
+            }
+
+            const requestOption = {
+                method: "POST",
+                headers: myHeaders,
+                body: JSON.stringify(body)
+            }
+
+            const response = await fetch(API_STAFF_LOGIN, requestOption);
+
+            const data = await response.json();
+
+            props.updateToken(data.token);
+
+            navigate("/staff");
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return ( 
+        <>
+            <Form>
+                <FormGroup>
+                    <Label for="email">Email</Label>
+                    <Input
+                        id="email"
+                        name="email"
+                        placeholder="Enter Email Addres"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="password">Password</Label>
+                    <Input
+                        id="password"
+                        name="password"
+                        placeholder="Enter Password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </FormGroup>
+                <Button title="Login" onClick={handleSubmit}>Login</Button>
+            </Form>
+        </>
+     );
+}
+ 
+export default Login;
